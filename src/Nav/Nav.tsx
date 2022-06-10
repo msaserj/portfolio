679aa9e859b7586f4c679681b5f8f0df12fa092c
msaserj <serg.ks@gmail.com>
2022-06-10 20:15:37 +0300

    01 start. Header and Nav

---
+++ src/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './Nav.module.css'
+
+export const Nav = () => {
+    return (
+        <div className={css.nav}>
+            <a href="">Main</a>
+            <a href="">Skills</a>
+            <a href="">Projects</a>
+            <a href="">Contacts</a>
+        </div>
+    );
+};
