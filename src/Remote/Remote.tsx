ab4b990325d42d64ebf563d098a5e9f94bb91cbd
msaserj <serg.ks@gmail.com>
2022-06-11 17:54:41 +0300

    05 Remote button

--- src/Remote/Remote.tsx
+++ src/Remote/Remote.tsx
@@ -10,7 +10,7 @@ export const Remote = () => {
         <div className={css.remoteBlock}>
             <div className={`${styleContainer.container} ${css.remoteContainer}`}>
                 <h2 className={css.title}>I'm considering remote work.</h2>
-                <button className={css.button}>Hire me.</button>
+                <button className={css.button}>Hire me</button>
             </div>
         </div>
     );
