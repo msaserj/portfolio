5447c2e6a4e0765c9cd8893f3a7408474a181296
msa <serg.ks@gmail.com>
2022-11-17 16:35:13 +0400

    style: header, photo

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import css from './Header.module.scss'
 import {Nav} from "../Nav/Nav";
+import photo from '../Common/assets/image/photo.jpg'
 
 export const Header = () => {
     return (
         <div className={css.header}>
+            <div className={css.photoBlock}>
+                <a href="https://msaserj.com">
+                    <img src={photo} alt="photo"/>
+                    <span>MSASERJ</span>
+                </a>
+            </div>
             <Nav/>
         </div>
     );
