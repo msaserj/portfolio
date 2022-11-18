ad19eae0c5f8f31797c84400dac50fe8d63ba3c9
msa <serg.ks@gmail.com>
2022-11-18 17:53:41 +0400

    style: main, pseudo line for container

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import css from './Header.module.scss'
 import {Nav} from "../Nav/Nav";
-import photo from '../Common/assets/image/photo.jpg'
+import photo from '../Common/assets/image/photo_fc.jpg'
 
 export const Header = () => {
     return (
         <div className={css.header}>
             <div className={css.photoBlock}>
                 <a href="https://msaserj.com">
-                    <img src={photo} alt="photo"/>
+                    <img src={photo} alt="MyPhoto"/>
                     <span>MSASERJ</span>
                 </a>
             </div>
