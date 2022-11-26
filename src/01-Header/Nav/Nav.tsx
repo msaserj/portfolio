397604c513badd3542efa913ba518c87c61ea5c0
msa <serg.ks@gmail.com>
2022-11-26 21:22:10 +0400

    style: nav react-scroll

--- src/01-Header/Nav/Nav.tsx
+++ src/01-Header/Nav/Nav.tsx
@@ -5,20 +5,13 @@ import {Link} from "react-scroll";
 export const Nav = () => {
     return (<>
         <ul className={css.nav}>
-            <li className={css.item}><a href="src/01-Header/Nav/Nav#home">HOME</a></li>
-            <li className={css.item}><a href="src/01-Header/Nav/Nav#skills">SKILLS</a></li>
-            <li className={css.item}><a href="src/01-Header/Nav/Nav#works">WORKS</a></li>
-            <li className={css.item}><a href="src/01-Header/Nav/Nav#resume">RESUME</a></li>
-            <li className={css.item}><a href="src/01-Header/Nav/Nav#blog">BLOG</a></li>
+            <Link activeClass={css.active} className={css.item} smooth={true} delay={50} to={"home"}>HOME</Link>
+            <Link activeClass={css.active} className={css.item} smooth={true} delay={50} to={"skills"}>SKILLS</Link>
+            <Link activeClass={css.active} className={css.item} smooth={true} delay={50} to={"works"}>WORKS</Link>
+            <Link activeClass={css.active} className={css.item} smooth={true} delay={50} to={"resume"}>RESUME</Link>
+            <Link activeClass={css.active} className={css.item} smooth={true} delay={50} to={"home"}>BLOG</Link>
             {/*<li className={css.item}><a href="#contacts">Contacts</a></li>*/}
-            <Link activeClass={css.active}
-                  className={css.item}
-                  spy={true}
-                  smooth={true}
-                  delay={50}
-                // isDynamic={true}
-                  ignoreCancelEvents={false}
-                  to={"contacts"}>CONTACTS</Link>
+            <Link activeClass={css.active} className={css.item} smooth={true} delay={50} to={"contacts"}>CONTACTS</Link>
         </ul>
     </>
 
--- src/08-Contacts/Contacts.tsx
+++ src/08-Contacts/Contacts.tsx
@@ -1,16 +1,12 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import css from './Contacts.module.scss'
 import styleContainer from '../00-Common/styles/Container.module.scss'
 import {Form} from "./work/Form";
 import {Title} from "../00-Common/components/title/Title";
 
 export const Contacts = () => {
-    const contactsToScroll = useRef<HTMLDivElement>(null)
-    useEffect(()=>{
-        contactsToScroll.current?.scrollIntoView({behavior: "smooth"})
-    }, [])
     return (
-        <div ref={contactsToScroll} id="contacts" className={css.contactsBlock}>
+        <div id="contacts" className={css.contactsBlock}>
             <div className={`${styleContainer.container} ${css.contactsContainer}`}>
                 <Title description={"contacts"} title={"Contact With Me"}/>
                 {/*<h1 className={css.title}>08-Contacts</h1>*/}
