f0e275c640879f9d3ee2714b9bd863e82bf0e28d
msa <serg.ks@gmail.com>
2022-11-27 17:25:48 +0400

    style: resume education layout

--- src/06-Resume/Education/Education.tsx
+++ src/06-Resume/Education/Education.tsx
@@ -4,14 +4,67 @@ import css from "./Educationl.module.scss"
 
 export const Education = () => {
     return (
-        <>
+        <div className={css.education}>
             <Slide triggerOnce direction={"up"}>
                 <h1>Educational</h1>
             </Slide>
             <div className={css.content}>
-                <div>1</div>
-                <div>2</div>
+                <div className={css.Tree}>
+                    <div className={css.treeBlock}>
+                        <div className={css.inner}>
+                            <h3>Block</h3>
+                            <h3>Block</h3>
+                            <p>Higher education is tertiary education leading to award of an academic degree.
+                                Higher education, also called post-secondary education.</p>
+                        </div>
+                    </div>
+                    <div className={css.treeBlock}>
+                        <div className={css.inner}>
+                            <h3>Block</h3>
+                            <h3>Block</h3>
+                            <p>Higher education is tertiary education leading to award of an academic degree.
+                                Higher education, also called post-secondary education.</p>
+                        </div>
+                    </div>
+                    <div className={css.treeBlock}>
+                        <div className={css.inner}>
+                            <h3>Block</h3>
+                            <h3>Block</h3>
+                            <p>Higher education is tertiary education leading to award of an academic degree.
+                                Higher education, also called post-secondary education.</p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className={css.Tree}>
+                    <div className={css.treeBlock}>
+                        <div className={css.inner}>
+                            <h3>Block</h3>
+                            <h3>Block</h3>
+                            <p>Higher education is tertiary education leading to award of an academic degree.
+                                Higher education, also called post-secondary education.</p>
+                        </div>
+                    </div>
+                    <div className={css.treeBlock}>
+                        <div className={css.inner}>
+                            <h3>Block</h3>
+                            <h3>Block</h3>
+                            <p>Higher education is tertiary education leading to award of an academic degree.
+                                Higher education, also called post-secondary education.</p>
+                        </div>
+                    </div>
+                    <div className={css.treeBlock}>
+                        <div className={css.inner}>
+                            <h3>Block</h3>
+                            <h3>Block</h3>
+                            <p>Higher education is tertiary education leading to award of an academic degree.
+                                Higher education, also called post-secondary education.</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </>
+        </div>
     );
 };
--- src/06-Resume/Resume.tsx
+++ src/06-Resume/Resume.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import css from './Resume.module.scss'
 import styleContainer from '../00-Common/styles/Container.module.scss'
-import {Res} from "./res/Res";
 import {Title} from "../00-Common/components/title/Title";
-import {TfiAlignJustify} from "react-icons/tfi";
 import {Slide} from "react-awesome-reveal";
 import {ResumeNav} from "./ResumeNav/ResumeNav";
 import {Route, Routes} from "react-router-dom";
@@ -13,35 +11,28 @@ import {Experience} from "./Experience/Experience";
 import {Interview} from "./Interview/Interview";
 
 export const Resume = () => {
-    const lorem = 'Lorem ipsum dolor sit amet, ' +
-        'consectetur adipiscing elit, ' +
-        'sed do eiusmod tempor incididunt ut ' +
-        'labore et dolore magna aliqua.'
+    // const lorem = 'Lorem ipsum dolor sit amet, ' +
+    //     'consectetur adipiscing elit, ' +
+    //     'sed do eiusmod tempor incididunt ut ' +
+    //     'labore et dolore magna aliqua.'
     return (
-        <div id={"resume"} className={css.skillsBlock}>
+        <div id={"resume"} className={css.resumeBlock}>
 
             <div className={`${styleContainer.container} ${css.resumeContainer}`}>
                 <Slide triggerOnce direction={"up"}>
                     <Title description={"5 + years experience"} title={"MY RESUME"}/>
                 </Slide>
-                <ResumeNav/>
-                <div className={css.contentBlock}>
-                    <Routes>
-                        <Route path="/education" element={<Education/>}/>
-                        <Route path="/profskills" element={<ProfSkills/>}/>
-                        <Route path="/experience" element={<Experience/>}/>
-                        <Route path="/interview" element={<Interview/>}/>
-                    </Routes>
+                <div className={css.contentContainer}>
+                    <ResumeNav/>
+                    <div className={css.contentBlock}>
+                        <Routes>
+                            <Route path="/education" element={<Education/>}/>
+                            <Route path="/profskills" element={<ProfSkills/>}/>
+                            <Route path="/experience" element={<Experience/>}/>
+                            <Route path="/interview" element={<Interview/>}/>
+                        </Routes>
 
-                </div>
-
-                <div className={css.skills}>
-                    <Res title={"HTML"} description={lorem}><TfiAlignJustify className={css.icon}/></Res>
-                    <Res title={"JS"} description={lorem}><TfiAlignJustify className={css.icon}/></Res>
-                    <Res title={"BlaBla"} description={lorem}><TfiAlignJustify className={css.icon}/></Res>
-                    <Res title={"GYGY"} description={lorem}><TfiAlignJustify className={css.icon}/></Res>
-                    <Res title={"AlyoBla"} description={lorem}><TfiAlignJustify className={css.icon}/></Res>
-                    <Res title={"AlyoBla"} description={lorem}><TfiAlignJustify className={css.icon}/></Res>
+                    </div>
                 </div>
             </div>
         </div>
