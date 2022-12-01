d2f51b4a51b24acb17b37af5faf75ee9edec8935
msa <serg.ks@gmail.com>
2022-12-01 15:23:26 +0400

    feat: add button up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import css from './App.module.scss';
 import {Header} from "./01-Header/Header";
 import {Main} from "./03-Main/Main";
 import {Skills} from "./04-Skills/Skills";
@@ -8,12 +8,14 @@ import {Remote} from "./07-Remote/Remote";
 import {Contacts} from "./08-Contacts/Contacts";
 import {Footer} from "./09-Footer/Footer";
 import {Resume} from "./06-Resume/Resume";
+import {FiArrowUp} from "react-icons/fi";
+import {Link} from "react-scroll";
 
 
 function App() {
 
     return (
-        <div className="App">
+        <div className={css.App}>
             <Header/>
             <Main/>
             <Skills/>
@@ -22,7 +24,16 @@ function App() {
             <Remote/>
             <Contacts/>
             <Footer/>
+
+
+            <Link spy activeClass={css.active} className={css.relative} smooth={true} delay={50} to={"home"}>
+                <div className={css.backToTop}>
+                    <FiArrowUp/>
+                </div>
+            </Link>
+
         </div>
+
     );
 }
 
