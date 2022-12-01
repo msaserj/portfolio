5ae204a538437b9cce4e06bd52aa29658cc9023b
msa <serg.ks@gmail.com>
2022-12-01 15:23:42 +0400

    refactor: contacts

--- src/08-Contacts/Contacts.tsx
+++ src/08-Contacts/Contacts.tsx
@@ -3,14 +3,15 @@ import css from './Contacts.module.scss'
 import styleContainer from '../00-Common/styles/Container.module.scss'
 import {Form} from "./work/Form";
 import {Title} from "../00-Common/components/title/Title";
+import {ContactsBlock} from "./ContactsBlock/ContactsBlock";
 
 export const Contacts = () => {
     return (
         <div id="contacts" className={css.contactsBlock}>
             <div className={`${styleContainer.container} ${css.contactsContainer}`}>
                 <Title description={"contacts"} title={"Contact With Me"}/>
-                {/*<h1 className={css.title}>08-Contacts</h1>*/}
                 <div className={css.contacts}>
+                    <ContactsBlock/>
                     <Form />
                 </div>
             </div>
---
+++ src/08-Contacts/ContactsBlock/ContactsBlock.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import css from "./ContactsBlock.module.scss"
+import {LiBtn} from "../../00-Common/components/LinkButton/LiBtn";
+import {FiFacebook, FiLinkedin, FiTwitter} from "react-icons/fi";
+
+export const ContactsBlock = () => {
+    const picSrc = "https://rainbowit.net/themes/inbio/wp-content/uploads/2021/08/contact1.png"
+    return (
+        <div className={css.contacts}>
+            <div className={css.imageBlock}>
+                <img className={css.image} src={picSrc} alt="connect"/></div>
+            <div>
+                <h2>Sergei Minko</h2>
+                <span className={css.description}>React Frontend Developer</span>
+            </div>
+            <div>
+                <p className={css.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div>
+                <span className={css.span}>Phone: <a className={css.a} href="tel: +123456789">+123456789</a></span>
+            </div>
+            <div>
+                <span className={css.span}>Email: <a className={css.a} href="mailto: serg.ks@gmail.com">serg.ks@gmail.com</a></span>
+            </div>
+            <div>
+                <h3>Find Me:</h3>
+                <ul className={css.links}>
+                    <LiBtn link={"https://www.linkedin.com/in/msaserj/"}><FiLinkedin /></LiBtn>
+                    <LiBtn link={"https://www.linkedin.com/in/msaserj/"}><FiFacebook /></LiBtn>
+                    <LiBtn link={"https://www.linkedin.com/in/msaserj/"}><FiTwitter /></LiBtn>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
