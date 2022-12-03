f31b57b922d76ab19a68ef9a924f12cff01a7bea
msa <serg.ks@gmail.com>
2022-12-03 20:12:48 +0400

    refactor: BestSkills and FindMeBlock to components

--- src/03-Main/Options/Options.tsx
+++ src/03-Main/Options/Options.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import css from "./Options.module.scss";
-import {LiBtn} from "../../00-Common/components/LinkButton/LiBtn";
-import {FiGithub, FiLinkedin, FiTwitter} from "react-icons/fi";
-import {FaReact} from "react-icons/fa";
-import {SiSass, SiTypescript} from "react-icons/si";
+import {FindMeBlock} from "../../00-Common/components/FindMeBlock/FindMeBlock";
+import {BestSkillsBlock} from "../../00-Common/components/BestSkillsBlock/BestSkillsBlock";
 
 export const Options = () => {
     return (
         <div className={css.options}>
-            <div>
-                <h3 className={css.optionsHeader}>FIND ME:</h3>
-                <ul className={css.ul}>
-                    <LiBtn title={"LinkedIn"} blank pointer link={"https://www.linkedin.com/in/msaserj"}><FiLinkedin/></LiBtn>
-                    <LiBtn title={"GitHub"} blank pointer link={"https://github.com/msaserj"}><FiGithub/></LiBtn>
-                    <LiBtn title={"Twitter"} blank pointer link={"https://www.twitter.com"}><FiTwitter/></LiBtn>
-                </ul>
-            </div>
-            <div>
-                <h3 className={css.optionsHeader}>BEST SKILLS:</h3>
-                <ul className={css.ul}>
-                    <LiBtn title={"React"}><FaReact style={{color: "#139cff"}}/></LiBtn>
-                    <LiBtn title={"SCSS"}><SiSass style={{color: "#cc0092"}}/></LiBtn>
-                    <LiBtn title={"TS"}><SiTypescript style={{color: "#0048ff"}}/></LiBtn>
-                </ul>
-            </div>
+            <FindMeBlock/>
+            <BestSkillsBlock/>
 
         </div>
     );
