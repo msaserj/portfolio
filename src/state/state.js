8c08516c3963c089adceb138bfd0f07db6391295
msa <serg.ks@gmail.com>
2022-12-06 00:33:10 +0400

    fix: state.json to state.js

---
+++ src/state/state.js
@@ -0,0 +1,223 @@
+const resume = {
+    education: {
+        left: {
+            years: "2000-2010",
+            title: "State Education",
+            items: [
+                {
+                    title: "Kostroma School of Secondary Education",
+                    years: "School of Secondary Education",
+                    rate: "2000-2003",
+                    description: "School of Secondary Education"
+                },
+                {
+                    title: "Kostroma Energy College named after F.V. Chizhov.",
+                    years: "Secondary vocational education.",
+                    rate: "2003-2006",
+                    description: " Installation, adjustment and operation of electrical equipment of industrial and civil buildings."
+                },
+                {
+                    title: "Kostroma State Agricultural Academy.",
+                    years: "Higher education school",
+                    rate: "2006-2010",
+                    description: "Engineer specializing in electrification and automation of agriculture."
+                }
+            ]
+        },
+        right: {
+            years: "2000-2022",
+            title: "Self Education",
+            items: [
+                {
+                    title: "Circle of radio electronics",
+                    years: "Extracurricular educations",
+                    rate: "2000-2002",
+                    description: "Radio electronics course."
+                },
+                {
+                    title: "Udemy: The Complete 2020 Web Development Bootcamp",
+                    years: "SWeb Development Bootcamp",
+                    rate: "2020-2021",
+                    description: "The main concepts of back-end and front-end web development are relevant for 2020."
+                },
+                {
+                    title: "IT-Incubator",
+                    years: "IT-Incubator Minsk, Belarus.",
+                    rate: "2022-2022",
+                    description: "IT incubator is not IT courses, but an incubator. Not only study but also practical team development of your own project."
+                }
+            ]
+        }
+    },
+    experience: {
+        left: {
+            years: "2011-2022",
+            title: "MY EXPERIENCE",
+            items: [
+                {
+                    title: "KGSXA",
+                    years: "2004-2008",
+                    rate: "3.90 / 4",
+                    description: "Lorem Ipsum"
+                },
+                {
+                    title: "Energy Tech",
+                    years: "2006-2008",
+                    rate: "3.90 / 5",
+                    description: "Lorem Ipsum"
+                },
+                {
+                    title: "School",
+                    years: "2004-2015",
+                    rate: "3.40 / 4",
+                    description: "Lorem Ipsum"
+                }
+            ]
+        },
+        right: {
+            years: "2007-2017",
+            title: "Self Education",
+            items: [
+                {
+                    title: "gfgfgf",
+                    years: "2004-6565",
+                    rate: "3.90 / 10",
+                    description: "Lorem Ipsum dolor sit amet, consectetur adipiscing elit."
+                },
+                {
+                    title: "Energy fgfgfgTech",
+                    years: "2006-2008",
+                    rate: "3.90 / 5",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac velit consequat, vestibulum purus eget, sodales leo. Fusce interdum ac arcu euismod tempus. "
+                },
+                {
+                    title: "School School",
+                    years: "2004-2015",
+                    rate: "3.40 / 4",
+                    description: "Lorem Ipsum"
+                }
+            ]
+        }
+    },
+    interview: {
+        left: {
+            years: "2007-2222",
+            title: "INTERVIEW",
+            items: [
+                {
+                    title: "KGSXA",
+                    years: "2004-2008",
+                    rate: "3.90 / 4",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac velit consequat, vestibulum purus eget, sodales leo. Fusce interdum ac arcu euismod tempus. "
+                },
+                {
+                    title: "Energy Tech",
+                    years: "2006-2008",
+                    rate: "3.90 / 5",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac velit consequat, vestibulum purus eget, sodales leo. Fusce interdum ac arcu euismod tempus. "
+                },
+                {
+                    title: "School",
+                    years: "2004-2015",
+                    rate: "3.40 / 4",
+                    description: "Lorem Ipsum"
+                }
+            ]
+        },
+        right: {
+            years: "2007-2018",
+            title: "Self Education",
+            items: [
+                {
+                    title: "gfgfgf",
+                    years: "2004-6565",
+                    rate: "3.90 / 10",
+                    description: "Lorem Ipsum"
+                },
+                {
+                    title: "Energy fgfgfgTech",
+                    years: "2006-2008",
+                    rate: "3.90 / 5",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac velit consequat, vestibulum purus eget, sodales leo. Fusce interdum ac arcu euismod tempus. "
+                },
+                {
+                    title: "School School",
+                    years: "2004-2015",
+                    rate: "3.40 / 4",
+                    description: "Lorem Ipsum"
+                }
+            ]
+        }
+    },
+    skills: {
+        left: {
+            years: "features",
+            title: "Development Skills",
+            items: [
+                {
+                    title: "HTML",
+                    percent: 90
+                },
+                {
+                    title: "CSS/SCSS",
+                    percent: 85
+                },
+                {
+                    title: "JS/TS",
+                    percent: 75
+                },
+                {
+                    title: "React",
+                    percent: 78
+                },
+                {
+                    title: "Redux",
+                    percent: 72
+                },
+                {
+                    title: "MaterialUI",
+                    percent: 80
+                },
+                {
+                    title: "Formik",
+                    percent: 80
+                }
+            ]
+        },
+        right: {
+            years: "features",
+            title: "Other Skills",
+            items: [
+                {
+                    title: "Webstorm",
+                    percent: 75
+                },
+                {
+                    title: "VSCode",
+                    percent: 65
+                },
+                {
+                    title: "Git",
+                    percent: 80
+                },
+                {
+                    title: "Storybook",
+                    percent: 75
+                },
+                {
+                    title: "LinuxMint",
+                    percent: 75
+                },
+                {
+                    title: "Debian",
+                    percent: 70
+                },
+                {
+                    title: "NodeJS",
+                    percent: 60
+                }
+            ]
+        }
+    }
+}
+export default resume;
