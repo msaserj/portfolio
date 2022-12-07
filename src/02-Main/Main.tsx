7361d7a0b0ec16f3f13322e01320233097c0540f
msa <serg.ks@gmail.com>
2022-12-07 14:11:39 +0400

    no photo

--- src/02-Main/Main.tsx
+++ src/02-Main/Main.tsx
@@ -4,7 +4,7 @@ import styleContainer from '../00-Common/styles/Container.module.scss'
 import {Slide} from "react-awesome-reveal";
 import {Options} from "./Options/Options";
 import {AboutMe} from "./AboutMe/AboutMe";
-import mainPhoto from "../00-Common/assets/image/mainphoto.jpg"
+
 
 
 export const Main = () => {
@@ -12,7 +12,9 @@ export const Main = () => {
         <section id={"home"} className={css.mainBlock}>
             <div className={`${styleContainer.container} ${css.mainContainer}`}>
                 <Slide triggerOnce direction={"down"}>
-                    <div className={css.photo}><img src={mainPhoto} alt="mainPhoto"/></div>
+                    <div className={css.photo}>
+                        {/*<img src={mainPhoto} alt="mainPhoto"/>*/}
+                    </div>
                 </Slide>
                 <Slide triggerOnce direction={"down"}>
                     <div className={css.aboutMe}>
