d8ec5d935035fca9902604d4cca053d882803a60
msa <serg.ks@gmail.com>
2022-12-16 03:46:27 +0400

    add: background-photo

--- src/02-Main/Main.tsx
+++ src/02-Main/Main.tsx
@@ -4,13 +4,15 @@ import styleContainer from '../00-Common/styles/Container.module.scss'
 import {Slide} from "react-awesome-reveal";
 import {Options} from "./Options/Options";
 import {AboutMe} from "./AboutMe/AboutMe";
-
+import bgp from "../00-Common/assets/image/code.jpg"
 
 
 export const Main = () => {
     return (
         <section id={"home"} className={css.mainBlock}>
+            <img className={css.bgp} src={bgp} alt=""/>
             <div className={`${styleContainer.container} ${css.mainContainer}`}>
+
                 <Slide triggerOnce direction={"down"}>
                     <div className={css.photo}>
                         {/*<img src={mainPhoto} alt="mainPhoto"/>*/}
