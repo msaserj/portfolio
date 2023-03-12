4049b19ee21e0a52c994c9f09e347637abf0787a
msa <serg.ks@gmail.com>
2023-03-12 18:13:41 +0400

    add: portfolio2

--- src/04-Works/00_WorksData/WorksData.js
+++ src/04-Works/00_WorksData/WorksData.js
@@ -165,7 +165,8 @@ const works = {
             "when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         gallery: [
             "https://msaserj.ru/portfolio/Dndcalc/dnd-calc2.png",
-
+            "https://msaserj.ru/portfolio/Dndcalc/dnd-calc2.png",
+            "https://msaserj.ru/portfolio/Dndcalc/dnd-calc2.png"
         ],
         addDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
             "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s," +
