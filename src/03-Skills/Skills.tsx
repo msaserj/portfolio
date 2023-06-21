8ec477a9ee0d115f498b213e176b57666e6f436b
msa <serg.ks@gmail.com>
2023-06-21 16:15:16 +0400

    feat: skill content

--- src/03-Skills/Skills.tsx
+++ src/03-Skills/Skills.tsx
@@ -3,35 +3,49 @@ import css from './Skills.module.scss';
 import styleContainer from '../00-Common/styles/Container.module.scss';
 import { Skill } from './skill/Skill';
 import { Title } from '../00-Common/components/title/Title';
-import { TfiAlignJustify, TfiLightBulb, TfiMobile } from 'react-icons/tfi';
-import { SiJest, SiLinux } from 'react-icons/si';
+import { TfiLightBulb, TfiMobile } from 'react-icons/tfi';
 import { FaPeopleCarry } from 'react-icons/fa';
+import {SlGraduation} from "react-icons/sl";
+import {CgPerformance} from "react-icons/cg";
+import {SiWebstorm} from "react-icons/si";
 
 export const Skills = () => {
   const lorem = [
     {
-      title: 'Responsive',
-      descr: 'My layouts will work on any device, big or small.'
+      title: 'Web Development',
+      descr: 'As a front-end developer, I specialize in creating engaging and interactive user interfaces ' +
+          'for websites and web applications. I am proficient in HTML, CSS, and JavaScript, ' +
+          'and have experience with modern frameworks and libraries like ReactJS, NextJS.'
     },
     {
-      title: 'Business Logic App',
-      descr: 'Business logic important aspect of development - it affects the speed of the application.'
+      title: 'Responsive Design',
+      descr: 'Websites and applications I develop are optimized for various devices and screen sizes.' +
+          ' I apply responsive design principles, allowing the content to adapt and provide an optimal' +
+          ' user experience across desktops, tablets, and mobile devices.'
     },
     {
-      title: 'Intuitive',
-      descr: 'Strong preference for easy to use, intuitive UX/UI.'
+      title: 'User Experience ',
+      descr: 'I focus on creating intuitive and user-friendly interfaces. ' +
+          'I collaborate with designers and stakeholders to translate wireframes and mockups into functional and ' +
+          'visually appealing user interfaces that prioritize usability and accessibility.'
     },
     {
-      title: 'Team Development',
-      descr: 'I have experience of working in a team and developing complex applications.'
+      title: 'Collaborative Development',
+      descr: 'I am experienced in working closely with back-end and front-end developers and designers ' +
+          'to achieve project goals. I effectively communicate and collaborate to understand requirements,' +
+          ' provide input on technical feasibility, and deliver high-quality front-end solutions.'
     },
     {
-      title: 'Unit Tests',
-      descr: 'Your app will be more stable and you will know whats broken in app'
+      title: 'Performance Optimization',
+      descr: 'I optimize the front-end code and assets to enhance the performance and ' +
+          'loading speed of websites and applications. I leverage techniques like code minification, ' +
+          'image compression, caching, and lazy loading to deliver optimal performance and improve user satisfaction.'
     },
     {
-      title: 'Linux',
-      descr: 'I use Linux systems for development, delivery and deployment of applications.'
+      title: 'Continuous Learning',
+      descr: ' I stay up-to-date with the latest front-end development trends, tools, and best practices. ' +
+          'I continuously expand my skill set and explore emerging technologies to deliver innovative ' +
+          'and cutting-edge solutions.'
     }
   ];
   return (
@@ -42,10 +56,10 @@ export const Skills = () => {
         </div>
         <div className={css.skills}>
           <Skill title={lorem[0].title} description={lorem[0].descr}>
-            <TfiMobile className={css.icon} />
+            <SiWebstorm className={css.icon} />
           </Skill>
           <Skill title={lorem[1].title} description={lorem[1].descr}>
-            <TfiAlignJustify className={css.icon} />
+            <TfiMobile className={css.icon} />
           </Skill>
           <Skill title={lorem[2].title} description={lorem[2].descr}>
             <TfiLightBulb className={css.icon} />
@@ -54,10 +68,10 @@ export const Skills = () => {
             <FaPeopleCarry className={css.icon} />
           </Skill>
           <Skill title={lorem[4].title} description={lorem[4].descr}>
-            <SiJest className={css.icon} />
+            <CgPerformance className={css.icon} />
           </Skill>
           <Skill title={lorem[5].title} description={lorem[5].descr}>
-            <SiLinux className={css.icon} />
+            <SlGraduation className={css.icon} />
           </Skill>
         </div>
       </div>
