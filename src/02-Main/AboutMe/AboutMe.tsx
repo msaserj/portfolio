d52b06a1a47377256420a7f58ec08252be69c89e
msa <serg.ks@gmail.com>
2023-06-21 17:40:19 +0400

    fix: AboutMe.tsx

--- src/02-Main/AboutMe/AboutMe.tsx
+++ src/02-Main/AboutMe/AboutMe.tsx
@@ -9,7 +9,7 @@ export const AboutMe = () => {
       <h3 className={css.header}>About Me</h3>
       <p>
         Hi I am a FrontEnd developer & I can build your website as you want. I have 1+ years of experience in Frontend
-        and building a SPA with React/Redux/TypeScript in a team, 2+ years of experience with HTML, CSS, JavaScript. 5+
+        and building a SPA with React/NextJS/TypeScript in a team, 2+ years of experience with HTML, CSS, JavaScript. 5+
         years of commercial experience in IT.
       </p>
       <p></p>
