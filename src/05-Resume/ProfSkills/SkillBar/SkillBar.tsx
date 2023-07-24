9160bb38023a31ffb621a4bf4c4ebf8f58a964b6
msa <serg.ks@gmail.com>
2023-07-24 22:06:49 +0400

    optimal SkillBar.tsx

--- src/05-Resume/ProfSkills/SkillBar/SkillBar.tsx
+++ src/05-Resume/ProfSkills/SkillBar/SkillBar.tsx
@@ -1,33 +1,30 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import css from './SkillBarr.module.scss';
-
-export type SkillBarType = {
+ export type SkillBarType = {
   title: string;
   percent: number;
 };
-
-export const SkillBar: React.FC<SkillBarType> = ({ title, percent }) => {
-  //const [counter, setCounter] = useState(20);
+ export const SkillBar: React.FC<SkillBarType> = ({ title, percent }) => {
   const ref = useRef<any>();
 
-  const increase = () => {
-    for (let i = 20; i < percent; i++) {
-      setTimeout(() => {
-        ref.current.style.setProperty(`width`, `${i}%`);
-      }, 1000);
-    }
-  };
-  // setTimeout(()=>{
-  //     counter <= percent && setCounter(counter+1)
-  // }, 10)
-
-  increase();
-
-  return (
+   useEffect(() => {
+    const startAnimation = () => {
+      let currentWidth = 20;
+      const animate = () => {
+        currentWidth += 1;
+        if (currentWidth <= percent) {
+          ref.current.style.setProperty('width', `${currentWidth}%`);
+          requestAnimationFrame(animate);
+        }
+      };
+      requestAnimationFrame(animate);
+    };
+     startAnimation();
+  }, [percent]);
+   return (
     <div className={css.skillBar}>
       <span className={css.header}>{title}</span>
-
-      <div className={css.progress}>
+       <div className={css.progress}>
         <div ref={ref} className={css.progressBar}>
           <div className={css.percent}>{percent} %</div>
         </div>
